9fc5dcc271117857813cd36c80e3c93199ae914b
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-04-05 14:56:19 -0300

    readme and application

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center">Validation Form</h1>
+<p align="center">Página de login com validações básicas feita em REACT</p>
+
+<h2>Funcionalidades</h2>
+<li>Verificar se o valor é email
+<li>Verificar se a senha tem 6 caracteres
+<li>Desabilita o campo de senha se o email estiver vazio
+<li>Desabilita o botão de login caso os valores estejam em branco
+
+<h3>Tecnologias e Ferramentas</h3>
+<li>React
+<li>Validator
+
+
+<h4>Conclusão</h4>
+<p>Não há todas as validações possíveis de um formulário, pois sempre dependerá da necessidade do seu projeto</p>
+
+<h5>Acesse a aplicação:</h5>
+<p></p>
+
+
+
+
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,19 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import validator from "validator";
 
 import "./App.css";
 
 function Form() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isTypingEmail, setIsTypingEmail] = useState(false);
+  const [isTypingPassword, setIsTypingPassword] = useState(false)
+
+  const validationEmail = (event) => {
+    let ema = event.target.value;
+    setEmail(ema);
+    setIsTypingEmail(true)
+    
+  };
+  const validationPassword = (event) => {
+    let pass = event.target.value;
+    setPassword(pass);
+    setIsTypingPassword(true)
+  };
   return (
     <>
       <header></header>
       <main>
         <div className="box">
           <label htmlFor="">Email:</label>
-          <input type="email" name="" id="email" />
+          <input
+            onChange={validationEmail}
+            type="email"
+            name=""
+            id="email"
+            value={email}
+          />
+          {isTypingEmail && !validator.isEmail(email) && (
+            <p>email Inválido</p>
+          )}
           <label htmlFor="">Password:</label>
-          <input type="password" name="" id="password" />
+          <input
+            disabled={email === ""}
+            onChange={validationPassword}
+            type="password"
+            name=""
+            id="password"
+            value={password}
+          />
+          {isTypingPassword && password.length < 6 && (<p>No minimo 6 digitos</p>) }
           <div className="align_button">
-            <button id="click">Login</button>
+            <button
+              disabled={email === "" || password === "" || password.length < 6}
+              id="click"
+            >
+              Login
+            </button>
           </div>
         </div>
       </main>
